1ae63694d09aeda14ec018e4ec41d5c1be13bcd2
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-06-18 13:24:05 +0200

    Remove author annotations

--- .spring_mantra.sh
+++ .spring_mantra.sh
@@ -116,9 +116,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.tinylog.Logger;
 
-/**
- * @author $gitCommitterName $gitCommitterSurname
- */
 @SpringBootApplication
 class Main {
 
@@ -146,9 +143,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author $gitCommitterName $gitCommitterSurname
- */
 @RestController
 @RequestMapping("/")
 class DefaultController {
